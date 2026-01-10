facb8eebafa45abacf44335db61efbae3ba63a17
Kaustubh Bhoir <kaustubhbhoir173@gmail.com>
2026-01-10 16:12:23 +0530

    Add authentication protection to job application route

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { applyForJob, getUserApplications, getJobApplicants } from "../controllers/applicationController.js";
 import upload from "../middleware/upload.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // POST apply for job (with CV upload)
-router.post("/apply", upload.single("cv"), applyForJob);
+router.post("/apply", protect, upload.single("cv"), applyForJob);
 
 // GET applications by user
 router.get("/user/:userId", getUserApplications);
